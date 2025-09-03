25247cc361d64764295a62862080fde76e866c99
gisman <gisman@gmail.com>
2025-09-03 18:55:50 +0900

    Dockerfile 추가: Python 3.13 소스 코드 기반 이미지 빌드 및 GIL 비활성화 설정; build.sh 파일 수정: 주석 처리된 docker tag 및 push 명령어

--- build.sh
+++ build.sh
@@ -6,10 +6,10 @@ echo "Docker 이미지 빌드 중..."
 docker build -t ${IMAGE_NAME}:${TAG} .
 
 # Tag the image for the local registry
-docker tag ${IMAGE_NAME}:${TAG} localhost:5300/${IMAGE_NAME}:${TAG}
+# docker tag ${IMAGE_NAME}:${TAG} localhost:5300/${IMAGE_NAME}:${TAG}
 
 # Push the image to the local registry
-docker push localhost:5300/${IMAGE_NAME}:${TAG}
+# docker push localhost:5300/${IMAGE_NAME}:${TAG}
 
 
 # Pull test
